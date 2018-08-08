48e70a6ef9ad8506256cfbb8861e3bc9b1a7e6fb
Gabriel <gabriel.bejarano@digitalcomtech.com>
2018-08-08 07:20:58 -0500

    fix tasks

--- src/pages/tasks/tasks.module.ts
+++ src/pages/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
 import { TasksPage } from "./tasks";
 import { PipesModule } from "../../pipes/pipes.module";
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
   declarations: [TasksPage],
-  imports: [IonicPageModule.forChild(TasksPage), PipesModule]
+  imports: [IonicPageModule.forChild(TasksPage), PipesModule, MomentModule]
 })
 export class TasksPageModule {}
